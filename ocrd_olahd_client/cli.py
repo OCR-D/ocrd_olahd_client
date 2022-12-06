f56d32c80892f9e8696f6d70f263eca4d414910f
joschrew <jonas.schrewe@gwdg.de>
2022-12-06 14:14:06 +0100

    workaround/reset default input-file-group

--- ocrd_olahd_client/cli.py
+++ ocrd_olahd_client/cli.py
@@ -6,4 +6,9 @@ from .processor import OlaHdClientProcessor
 @click.command()
 @ocrd_cli_options
 def cli(*args, **kwargs):
+    # workaround for default input_input_file_grp: by default input_file_grp is set to 'INPUT' by
+    # core because usually every processor has at least one input-file-grp. In case of this
+    # processor this can cause an error so it is just removed for now
+    if kwargs['input_file_grp'] == 'INPUT':
+        kwargs['input_file_grp'] = ''
     return ocrd_cli_wrap_processor(OlaHdClientProcessor, *args, **kwargs)
