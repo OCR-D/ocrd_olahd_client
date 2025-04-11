a6567f3f0f5798e96262dfbd49f014163d9e999d
kba <unixprog@gmail.com>
2025-04-11 17:55:22 +0200

    port to v3

--- Dockerfile
+++ Dockerfile
@@ -3,20 +3,26 @@ FROM $DOCKER_BASE_IMAGE
 ARG VCS_REF
 ARG BUILD_DATE
 LABEL \
-    maintainer="https://ocr-d.de/kontakt" \
+    maintainer="https://ocr-d.de/en/contact" \
     org.label-schema.vcs-ref=$VCS_REF \
     org.label-schema.vcs-url="https://github.com/OCR-D/ocrd_olahd_client" \
-    org.label-schema.build-date=$BUILD_DATE
+    org.label-schema.build-date=$BUILD_DATE \
+    org.opencontainers.image.vendor="DFG-Funded Initiative for Optical Character Recognition Development" \
+    org.opencontainers.image.title="ocrd_olahd_client" \
+    org.opencontainers.image.description="Client for OLA-HD" \
+    org.opencontainers.image.source="https://github.com/OCR-D/ocrd_olahd_client" \
+    org.opencontainers.image.documentation="https://github.com/OCR-D/ocrd_olahd_client/blob/${VCS_REF}/README.md" \
+    org.opencontainers.image.revision=$VCS_REF \
+    org.opencontainers.image.created=$BUILD_DATE \
+    org.opencontainers.image.base.name=ocrd/core
 
 WORKDIR /build/ocrd_olahd_client
-COPY setup.py .
-COPY ocrd_olahd_client/ocrd-tool.json .
-COPY ocrd_olahd_client ./ocrd_olahd_client
-COPY requirements.txt .
-COPY README.md .
-COPY Makefile .
-RUN make install
-RUN rm -rf /build/ocrd_olahd_client
+COPY . .
+COPY ocrd-tool.json .
+# prepackage ocrd-tool.json as ocrd-all-tool.json
+RUN ocrd ocrd-tool ocrd-tool.json dump-tools > $(dirname $(ocrd bashlib filename))/ocrd-all-tool.json
+# install everything and reduce image size
+RUN pip install . && rm -rf /build/ocrd_olahd_client
 
 WORKDIR /data
-VOLUME ["/data"]
+VOLUME /data
--- ocrd_olahd_client/processor.py
+++ ocrd_olahd_client/processor.py
@@ -1,45 +1,39 @@
-import json
+from functools import cached_property
 from os.path import join
 from tempfile import gettempdir
 from time import time
 
-from pkg_resources import resource_string
-
-from ocrd import Processor, Resolver
-from ocrd_utils import getLogger
+from ocrd import Processor, Resolver, Workspace
 from ocrd.workspace_bagger import WorkspaceBagger
 
 from .client import OlaHdClient
 
-OCRD_TOOL = json.loads(resource_string(__name__, 'ocrd-tool.json').decode('utf8'))
-TOOL = 'ocrd-olahd-client'
-
 
 class OlaHdClientProcessor(Processor):
 
-    def __init__(self, *args, **kwargs):
-        kwargs['ocrd_tool'] = OCRD_TOOL['tools'][TOOL]
-        kwargs['version'] = OCRD_TOOL['version']
-        super().__init__(*args, **kwargs)
+    @cached_property
+    def executable(self):
+        return 'ocrd-olahd-client'
 
-    def process(self):
-        LOG = getLogger('processor.OlaHdClientProcessor')
-        LOG.info('Posting workspace to %s' % self.parameter['endpoint'])
+    def process_workspace(self, workspace: Workspace) -> None:
+        assert self.parameter
+        self.logger.info('Posting workspace to %s' % self.parameter['endpoint'])
         client = OlaHdClient(self.parameter['endpoint'], self.parameter['username'],
                              self.parameter['password'])
         bagger = WorkspaceBagger(Resolver(), strict=True)
         # TODO
         dest = join(gettempdir(), 'bag-%d.ocrd.zip' % int(round((time() * 1000))))
         # TODO
-        ocrd_identifier = self.workspace.mets.unique_identifier
-        LOG.debug('Bagging workspace')
-        bagger.bag(self.workspace, ocrd_identifier, dest=dest)
-        LOG.debug('Logging in')
+        ocrd_identifier = workspace.mets.unique_identifier
+        self.logger.debug('Bagging workspace')
+        bagger.bag(workspace, ocrd_identifier, dest=dest)
+        self.logger.debug('Logging in')
         client.login()
-        LOG.debug('POST bag')
+        self.logger.debug('POST bag')
         prev_pid = self.parameter.get('pid_previous_version', None)
         pid = client.post(dest, prev_pid=prev_pid)
-        LOG.info(f"finished POST bag. Received PID: {pid}")
+        self.logger.info(f"finished POST bag. Received PID: {pid}")
+        # TODO: what's the purpose of this?
         if "password" in self.parameter:
             self.parameter["password"] = "*****"
         if pid:
