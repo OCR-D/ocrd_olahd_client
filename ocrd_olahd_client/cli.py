39a3b756e1ed98c90694ee43e9deb00bdf425fae
Robert Sachunsky <38561704+bertsky@users.noreply.github.com>
2025-04-15 14:32:31 +0200

    v3-compatible workaround for not having an output fileGrp

--- ocrd_olahd_client/cli.py
+++ ocrd_olahd_client/cli.py
@@ -6,4 +6,7 @@ from .processor import OlaHdClientProcessor
 @click.command()
 @ocrd_cli_options
 def cli(*args, **kwargs):
+    # OCR-D CLI spec requires input, which the processor wrapper enforces (not empty, not None)
+    # but we can pass an empty string as Processor.verify will not check its existence
+    kwargs['input_file_grp'] = ''
     return ocrd_cli_wrap_processor(OlaHdClientProcessor, *args, **kwargs)
